633818fec7426791e0aeb13461582a733b21a2c1
Sumon Paul <sumonpaul461@gmail.com>
2022-11-22 12:49:41 +0600

    add css & java file

---
+++ JavaProgramming/package bd.com.howtocode.java.tutotorial.java
@@ -0,0 +1,13 @@
+ package bd.com.howtocode.java.tutotorial.syntax;
+
+        public class HelloWorld {
+            public static void main(String[] args) {
+                System.out.println("Hello, world!"); // Advance the cursor to the beginning of next line after printing
+                System.out.println();                // Print a empty line
+                System.out.print("Hello, world!");   // Cursor stayed after the printed string
+                System.out.println("Hello,");
+                System.out.print(" ");               // Print a space
+                System.out.print("world!");
+                System.out.println("Hello, world!");
+            }
+        }
